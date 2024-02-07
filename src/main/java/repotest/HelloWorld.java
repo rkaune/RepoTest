a851464a2dd40ea7fce4d244e218a34fa06cdc5c
RossICS2022 <119336782+RossICS2022@users.noreply.github.com>
2024-02-07 13:42:42 -0500

    Second commit
    added SOP

--- src/main/java/repotest/HelloWorld.java
+++ src/main/java/repotest/HelloWorld.java
@@ -15,6 +15,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Next line");
     }
     
 }
